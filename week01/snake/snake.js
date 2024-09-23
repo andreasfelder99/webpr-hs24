4968e526e892b3344ebb071f11733c4807bb6f35
Andreas Felder <andreas.felder@students.fhnw.ch>
2024-09-23 14:06:12 +0200

    snake done

--- week01/snake/snake.js
+++ week01/snake/snake.js
@@ -17,12 +17,13 @@ const snake = [
 ];
 let food = {x: 15, y: 15};
 
-function snakeEquals(a, b) { 
-	/* fill here */
+function snakeEquals(a, b) {
+    return a.x === b.x && a.y === b.y
 }
 
 function changeDirection(orientation) {
-    /* fill here */
+    const idx = orientation.indexOf(direction);
+    direction = orientation[idx + 1];
 }
 
 function start() {
@@ -62,10 +63,10 @@ function nextBoard() {
         food.x = Math.floor(Math.random() * 20);   // place new food at random location
         food.y = Math.floor(Math.random() * 20);
     } else {
-        /* fill here */ // no food found => no growth despite new head => remove last element
+        snake.pop();
     }
 
-    /* fill here */ // put head at front of the list
+    snake.unshift(head);
 }
 
 function display(context) {
